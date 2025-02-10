53410d24f6bdf6b085446fc56594d83d1e8a85dc
Big Brother <aykibugz@yandex.ru>
2025-02-10 16:48:25 +0400

    Stoppiiieee

---
+++ download.py
@@ -0,0 +1,41 @@
+﻿import os
+import requests
+from secrets import token_hex
+
+from config import *
+
+def download_slides(url: str, start: int = 1, end: int = None) -> str:
+    url = os.path.join(url, "svg/", "{index}")
+    
+    index = start
+
+    while True:
+        dirname = os.path.join(OUTDIR, f"slides_{token_hex(nbytes=3)}")
+
+        if os.path.exists(dirname): 
+            continue
+        
+        os.mkdir(dirname)
+        break
+
+
+    while True:
+        if end is not None and index == end + 1:
+            break
+
+        res = requests.get(url.format(index=index), stream=True, timeout=5)
+
+        print(res.request.url)
+
+        if res.status_code == 404:
+            break
+
+        filename = os.path.join(dirname, f"slide_{index}.svg")
+        print(filename)
+        with open(filename, "wb") as file:
+            for chunk in res:
+                file.write(chunk)
+
+        index += 1
+    
+    return dirname
---
+++ main.py
@@ -0,0 +1,37 @@
+﻿import argparse
+import os
+
+from config import *
+from download import download_slides
+
+def _parse_arguments():
+    parser = argparse.ArgumentParser(
+        prog="main.py",
+        description="DownloadS slides from BBB"
+    )
+
+    parser.add_argument("url", type=str, help="URL like 'https://bbb123.ssau.ru/bigbluebutton/presentation/aa788..f9ed-173..849/aa78..f9ed-173..49/c41..34d-1..4/'")
+    parser.add_argument("start", type=int, nargs="?", help="Start slide index.")
+    parser.add_argument("end", type=int, nargs="?", help="End slide index.")
+    
+    return parser.parse_args()
+
+
+
+if __name__ == "__main__":
+    args = _parse_arguments()
+
+    if not os.path.exists(OUTDIR):
+        os.mkdir(OUTDIR)
+
+    try:
+        path_svg = download_slides(
+            args.url, 
+            (args.start, 1)[args.start is None], 
+            args.end
+        )
+    except Exception as e:
+        print(e)
+        exit()
+
+    print(f"Download to: {path_svg}")
